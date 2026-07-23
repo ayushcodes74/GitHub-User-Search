829272a8e0a0a0f848b9e4812a2148daaf79be84
Ayush Raj <rajpiyush091@gmail.com>
2026-07-24 03:50:17 +0530

    Create GitHub user search input component with state handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,24 @@
+import React from "react";
+import { useState } from "react"
 
+function App () {
+  const [search,setSearch]=useState("")
+
+  function handleSubmit() {
+   console.log(search)
+   setSearch("")
+  }
+
+  function handleChange(event) {
+         setSearch(event.target.value)
+  }
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <input type="text" placeholder="Enter Username" onChange={handleChange} value={search}></input>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+
+}
+export default App;
